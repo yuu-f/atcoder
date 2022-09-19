affe3813e74a92bb2c590da8c2ed4767e0e79228
yuu-f <rororo3434@yahoo.co.jp>
2022-09-19 13:28:01 +0000

    Update template 20220919

--- temp.cpp
+++ temp.cpp
@@ -19,7 +19,7 @@ using pii = pair<int,int>;
 
 #define rep(i,a,b) for(int i=a;i<b;i++)
 #define rrep(i,a,b) for(int i=a;i>=b;i--)
-#define all(x) (x.begin(),(x).end())
+#define all(x) x.begin(),(x).end()
 #define sz(x) ((int)(x).size())
 #define fore(i,a) for(auto &i:a)
 
