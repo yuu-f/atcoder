6cfeb0f94e4aa1531d17e703e8944d8a73c10617
yuu-f <rororo3434@yahoo.co.jp>
2022-09-19 13:29:02 +0000

    20220919

---
+++ ABC077/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int,int>;
+
+#define pb push_back
+#define mp make_pair
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define all(x) (x.begin(),(x).end())
+#define sz(x) ((int)(x).size())
+#define fore(i,a) for(auto &i:a)
+
+int main(){
+  ll n;
+  cin>>n;
+  
+  rep(i,1,1000000){
+    if(n<i*i){
+      cout<<(i-1)*(i-1);
+      return 0;
+    }
+  }
+}
