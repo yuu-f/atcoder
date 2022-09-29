82a82e14a5dff2d433e35d7d2ec47b093383da89
yuu-f <rororo3434@yahoo.co.jp>
2022-09-29 05:34:50 +0000

    20220929

---
+++ ABC269/C.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int,int>;
+
+#define pb push_back
+#define mp make_pair
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define all(x) x.begin(),(x).end()
+#define sz(x) ((int)(x).size())
+#define fore(i,a) for(auto &i:a)
+
+#define _GLIBCXX_DEBUG
+
+int main(){
+    ll n;
+    cin>>n;
+    
+    vll x={0};
+    rep(i,0,60){
+        if((n&(1ll<<i))){
+            int len=sz(x);
+            rep(j,0,len){
+                x.pb(x[j]|(1ll<<i));
+            }
+        }
+    }
+    rep(i,0,sz(x)){
+        cout<<x[i]<<endl;
+    }
+}
